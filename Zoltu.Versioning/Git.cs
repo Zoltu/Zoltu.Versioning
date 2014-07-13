ac19b905ffd7760fe6526b245151a9cc3f4168db
Micah <micah@zoltu.net>
2014-07-12 23:46:11 -0700

    Adds versioning from git repository via LibGit2.

--- Zoltu.Versioning/Git.cs
+++ Zoltu.Versioning/Git.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -12,22 +14,70 @@ namespace Zoltu.Versioning
 		[Required]
 		public String OutputFilePath { get; set; }
 
+		private static Regex tagMatcher = new Regex(@"^v(\d+)\.(\d+)", RegexOptions.Compiled);
+
 		public override Boolean Execute()
 		{
 			Contract.Assume(Log != null);
 
-			var version = GetVersionFromGit();
+			var repositorySearchPathStart = Path.GetDirectoryName(OutputFilePath);
+			var version = GetVersionFromGit(repositorySearchPathStart);
 			var fileContents = GenerateVersionFileContents(version);
 			File.WriteAllText(OutputFilePath, fileContents);
 
 			return true;
 		}
 
-		public static String GetVersionFromGit()
+		public static String GetVersionFromGit(String repositorySearchPathStart)
 		{
-			Contract.Ensures(Contract.Result<String>() != null);
+			Contract.Requires(repositorySearchPathStart != null);
 
-			return "1.2.3.4";
+			var repositoryPath = LibGit2Sharp.Repository.Discover(repositorySearchPathStart);
+			using (var repository = new LibGit2Sharp.Repository(repositoryPath))
+			{
+				var tag = GetVersionTagFromGit(repository);
+				var commitCount = (tag == null)
+					? GetCommitCountSinceStart(repository)
+					: GetCommitCountSinceTag(repository, tag);
+
+				var tagMatch = tagMatcher.Match(tag.Name);
+				var majorVersion = tagMatch.Groups[1].Value;
+				var minorVersion = tagMatch.Groups[2].Value;
+
+				return String.Format(@"{0}.{1}.{2}.0", majorVersion, minorVersion, commitCount);
+			}
+		}
+
+		public static Int32 GetCommitCountSinceStart(LibGit2Sharp.IRepository repository)
+		{
+			Contract.Requires(repository != null);
+			Contract.Ensures(Contract.Result<Int32>() >= 0);
+
+			return repository.Head.Commits.Count();
+		}
+
+		public static Int32 GetCommitCountSinceTag(LibGit2Sharp.IRepository repository, LibGit2Sharp.Tag tag)
+		{
+			Contract.Requires(repository != null);
+			Contract.Requires(tag != null);
+			Contract.Ensures(Contract.Result<Int32>() >= 0);
+
+			return repository.Head.Commits
+				.TakeWhile(commit => commit.Sha != tag.Target.Sha)
+				.Count();
+		}
+
+		private static LibGit2Sharp.Tag GetVersionTagFromGit(LibGit2Sharp.IRepository repository)
+		{
+			Contract.Requires(repository != null);
+
+			var tags = repository.Tags.ToDictionary(tag => tag.Target.Sha);
+
+			return repository.Head.Commits
+				.Where(commit => tags.ContainsKey(commit.Sha))
+				.Select(commit => tags[commit.Sha])
+				.Where(tag => tagMatcher.IsMatch(tag.Name))
+				.FirstOrDefault();
 		}
 
 		public static String GenerateVersionFileContents(String version)
